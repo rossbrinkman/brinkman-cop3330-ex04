b322a7e4cb385b57c0cd08999b693e8c39cbdb89
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-10 11:12:33 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Ross Brinkman
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "What is the quote?" );
+
+        Scanner scanner = new Scanner(System.in);
+        String quote = scanner.nextLine();
+        String author;
+
+        System.out.println( "Who said it?" );
+    }
+}
